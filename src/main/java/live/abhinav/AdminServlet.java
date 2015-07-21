e2d0b3bd157569b05819384bed6eb6436219ad55
anandAbhi <oct94abhi@gmail.com>
2015-07-21 15:12:45 +0530

    Initial commit Befor recaptcha

---
+++ src/main/java/live/abhinav/AdminServlet.java
@@ -0,0 +1,20 @@
+package live.abhinav; /**
+ * Created by abhin on 7/20/2015.
+ */
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns ={"/admin" } )
+public class AdminServlet extends HttpServlet {
+    @Override
+    public void doGet (HttpServletRequest request , HttpServletResponse response){
+        try {
+            response.getWriter( ).write( "This is accessible to ADMIN only" ) ;
+        } catch(IOException e) {
+            e.printStackTrace( ) ;
+        }
+    }
+}
---
+++ src/main/java/live/abhinav/controller/LoginController.java
@@ -0,0 +1,18 @@
+package live.abhinav.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by abhin on 7/21/2015.
+ */
+
+@Controller
+@RequestMapping("")
+public class LoginController {
+    @RequestMapping(method = RequestMethod.GET, value = "/custom_login")
+    public String showLogin() {
+        return "login";
+    }
+}
